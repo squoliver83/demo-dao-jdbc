6f2153af506abf51e664eb889ed3f6ce9d8742b3
Samuel Quaresma <squoliver@gmail.com>
2022-01-07 11:48:35 -0300

    Seller entity class

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,6 +1,7 @@
 package model.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Department implements Serializable {
 	
@@ -30,14 +31,11 @@ public class Department implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
-	}
+	}	
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -49,12 +47,7 @@ public class Department implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		if(id == null ) {
-			if(other.id != null)
-				return false;
-		} else if(!id.equals(other.id))
-			return false;
-		return true;
+		return Objects.equals(id, other.id);
 	}
 
 	@Override
